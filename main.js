de12bfa80e7f95f39ebf5ec834d087c5bf61613b
Laura-Martinez182 <lauramartinezo182@gmail.com>
2022-03-18 21:49:15 -0500

    IT09-12-Implementar-funcionalidad-cerrar-issue

--- main.js
+++ main.js
@@ -45,16 +45,31 @@ function fetchIssues() {
             var status = issues[i].status;
 
             issuesList.innerHTML += '<div class="well">' +
-                                    '<h6>Issue ID: ' + id + '</h6>' +
-                                    '<p><span class="label label-info">' + status + '</span></p>' +
-                                    '<h3>' + desc + '</h3>' +
-                                    '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
-                                    '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
-                                    '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\"id"\')">Close</a>' +
-                                    '</div>';
+                '<h6>Issue ID: ' + id + '</h6>' +
+                '<p><span class="label label-info">' + status + '</span></p>' +
+                '<h3>' + desc + '</h3>' +
+                '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
+                '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
+                '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a>' +
+                '</div>';
         }
     }
 }
+
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = "Closed";
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
 
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
