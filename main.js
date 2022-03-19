44d0f28b233cb435e3f884411292acf8b85d8107
Luis Miguel Ossa Arias <1006016025@u.icesi.edu.co>
2022-03-18 21:56:28 -0500

    IT09-14-Crear-botón-eliminar-issue

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ function fetchIssues() {
                 '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
                 '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
                 '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a>' +
+                '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'
                 '</div>';
         }
     }
